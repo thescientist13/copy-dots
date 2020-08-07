39c8bbc67f6b90680108b132461df7605b803e40
Owen Buckley <owenbuckley13@gmail.com>
2020-08-06 21:21:24 -0400

    basic POC

---
+++ README.md
@@ -0,0 +1,34 @@
+# copy dots
+
+## Overview
+
+Quickly copy dot files (and more) from a previous project into a new project with a single command.
+
+## Installation (TODO)
+
+TBD (node version, etc)
+
+## Usage
+
+**copy dota** has one required paramater, and can take a number of options.
+
+### Basic
+
+**copy-dots** at least neads to know what path to copy from.
+
+```sh
+$ npx copy-dots ../../my-project
+```
+
+### Options (TODO)
+
+**copy-dots** also support the following options to copy additional files.
+
+- -l (License), 
+- -g (git)
+- -r (readme), 
+- -w (all the flags)
+- -c (config files)
+
+## Contributing
+TODO
--- src/index.js
+++ src/index.js
@@ -1,1 +1,102 @@
-console.log('hello world');
+#!/usr/bin/env node
+/* eslint no-console: 0 */
+
+const fs = require('fs');
+const path = require('path');
+
+function filterForDotFiles(files = []) {
+  console.log('filterForDotFiles', files);
+  return files.filter(file => {
+    console.log('file', file);
+    if (file.indexOf('.') === 0) {
+      return file;
+    };
+  })
+} 
+
+const filterFiles = (files = [], options = {}) => {
+  console.log('filter for files ????', files);
+  return new Promise((resolve, reject) => {
+    try {
+      const filteredFiles = filterForDotFiles(files);
+        
+      resolve(filteredFiles);
+    } catch(error) {
+      console.log('Unexpected error filtering files', error);
+      reject();
+    }
+  });
+}
+
+const scanFiles = (targetDirectory) => {
+  return new Promise((resolve, reject) => {
+    try {   
+      const files = fs.readdirSync(targetDirectory);
+      
+      resolve(files);
+    } catch(error) {
+      console.log('Unexpected error scanning files', error);
+    }
+  });
+}
+
+const validateUserPath = (args) => { 
+  return new Promise((resolve, reject) => {
+    if (args.length <= 2) {
+      reject('Missing required paramater: path.  Usage is npx copy-dots /some/path');
+    }
+
+    const absPath = path.join(process.cwd(), args[2]);
+
+    resolve(absPath);
+  });
+};
+
+const run = async () => {
+  try {
+    let targetDirectory = process.cwd();
+    console.log('Validating target directory...');
+    console.log('params', process.argv);
+    const userPathAbsolute = await validateUserPath(process.argv);
+
+    console.log('Scanning Files in user directory...');
+    console.log('userPathAbsolute', userPathAbsolute);
+    const files = await scanFiles(userPathAbsolute);
+
+    console.log('Filtering files in target directory...');
+    console.log('files', files);
+    const filteredFiles = await filterFiles(files);
+
+    console.log('Copying filtered files into current directory...');
+    console.log('========= FILTERED FILES', filteredFiles);
+
+    if (process.env.NODE_ENV === 'development') {
+      fs.mkdirSync(path.join(process.cwd(), './output'));
+    }
+
+    filteredFiles.forEach(file => {
+      const fullPath = `${userPathAbsolute}/${file}`;
+
+      if (process.env.NODE_ENV === 'development') {
+        targetDirectory = path.join('./output', file);
+      }
+  
+      console.log('copying file from', fullPath);
+      console.log('copying file to', targetDirectory);
+      fs.copyFileSync(`${fullPath}`, targetDirectory);
+    })
+
+    // // success!
+    // console.log('-------------------------------------------------------');
+    // console.log('Success, your project is ready to go!');
+    // console.log(`Just run: cd ${TARGET_DIR}`);
+    // console.log('And then launch your project with: npm start');
+    // console.log('-------------------------------------------------------');
+  } catch (err) {
+    console.error(err);
+  }
+
+  process.exit(); // eslint-disable-line no-process-exit
+};
+
+run();
