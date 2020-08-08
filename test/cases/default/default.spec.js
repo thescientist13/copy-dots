8145c1f3ef08932db7d54775e20dca91157f2c94
Owen Buckley <owenbuckley13@gmail.com>
2020-08-07 20:42:15 -0400

    update specs and test bed configuration

--- test/cases/default/default.spec.js
+++ test/cases/default/default.spec.js
@@ -11,28 +11,37 @@
  * User Workspace
  * Default fixtures
  */
-
 const expect = require('chai').expect;
-const fs = require('fs');
+const fs = require('fs-extra');
 const path = require('path');
+const rimraf = require('rimraf');
 const TestBed = require('../../test-bed');
 
 describe('Run copy-dots', function() {
-  const basePath = path.join(__dirname, './output');
   let bed;
+  let basePath;
 
   before(async function() {
+    await fs.mkdir(path.join(__dirname, './output'));
+
+    basePath = path.join(__dirname, './output');
     bed = new TestBed();
-    await bed.setup(__dirname);
+
+    await bed.setup(basePath);
   });
 
   describe('default options with relative path', function() {
     before(async function() {
-      await bed.runCommand('./src/index.js', '../../fixtures');
+      console.log('basepath', basePath);
+      await bed.runCommand(
+        './src/index.js', 
+        path.join(process.cwd(), './test/fixtures')
+      );
     });
 
     it('should only copy 3 files', function() {
       const files = fs.readdirSync(basePath);
+     
       expect(files.length).to.be.equal(3);
     });
 
@@ -47,10 +56,10 @@ describe('Run copy-dots', function() {
     it('should have .mocharc.js file', function() {
       expect(fs.existsSync(`${basePath}/.mocharc.js`)).to.be.equal(true);
     });
-
   });
 
   after(function() {
     // bed.teardown();
+    rimraf.sync(basePath);
   });
 });
